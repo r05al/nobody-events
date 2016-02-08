f2268847d1102ee60c40cd9bda637bdf87c15fe3
Peter Rosal <peter.rosal@gmail.com>
2016-02-08 18:53:51 -0500

    Add routes for registration and login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var passport = require('passport');
 var Event = mongoose.model('Event');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -94,6 +96,40 @@ router.put('/events/:event/comments/:comment/upvote', function(req, res, next){
 
     res.json(comment);
   });
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    if(err) { return next(err); }
+
+    return res.json({ token: user.generateJWT()});
+  });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err) { return next(err) }
+
+    if(user) {
+      return res.json({ token: user.generateJWT() });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
