3f3f9d0db88558114eb3b5ed5c6ef184238ce380
Peter Rosal <peter.rosal@gmail.com>
2016-02-09 15:47:32 -0500

    Add methods to attend and cancel

--- models/Events.js
+++ models/Events.js
@@ -3,13 +3,24 @@ var mongoose = require('mongoose');
 var EventSchema = new mongoose.Schema({
   title: String,
   location: String,
-  // attendants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  attendants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   upvotes: {type: Number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
 EventSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
+  this.save(cb);
+};
+
+EventSchema.methods.addAttendant = function(user, cb) {
+  this.attendants.addToSet(user);
+  this.save(cb);
+};
+
+EventSchema.methods.removeAttendant = function(user, cb) {
+  this.attendants.pull(user);
   this.save(cb);
 };
 
