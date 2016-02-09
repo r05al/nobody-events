09d474293aa21e0bf13e65bf3c137d910a0298d5
Peter Rosal <peter.rosal@gmail.com>
2016-02-08 19:23:27 -0500

    Add auth functions

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -43,6 +43,55 @@ app.factory('events', ['$http', function($http){
 
   return e;
 
+}]).factory('auth', ['$http', '$window', function($http, $window){
+  var auth = {};
+  
+  auth.saveToken = function(token) {
+    $window.localStorage['nobody-events-token'] = token;
+  };
+
+  auth.getToken = function() {
+    return $window.localStorage['nobody-events-token'];
+  }
+
+  auth.isLoggedIn = function() {
+    var token = auth.getToken();
+
+    if(token) {
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function() {
+    if(auth.isLoggedIn()) {
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user) {
+    return $http.post('/register', user).success(function(data) {
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user) {
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function(user) {
+    $window.localStorage.removeItem('nobody-events-token');
+  };
+
+  return auth;
 }]);
 
 app.controller('MainCtrl', [
@@ -88,7 +137,29 @@ app.controller('MainCtrl', [
     };
 
   }
-]);
+]).controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'auth',
+  function($scope, $state, auth) {
+    $scope.user = {};
+    
+    $scope.register = function() {
+      auth.register($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
+    }
+    
+    $scope.logIn = function() {
+      auth.logIn($scope.user).error(function(error){
+        $scope.error = error;
+      }).then(function(){
+        $state.go('home');
+      });
+    };
+  }]);
 
 app.config([
   '$stateProvider',
