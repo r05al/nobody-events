0c906dda3a705a35159a974ae8df78b15742eef9
Peter Rosal <peter.rosal@gmail.com>
2016-02-10 09:18:20 -0500

    Update schema fields

--- models/Comments.js
+++ models/Comments.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
   body: String,
-  author: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   upvotes: { type: Number, default: 0 },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' }
+  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' }
 });
 
 CommentSchema.methods.upvote = function(cb) {
